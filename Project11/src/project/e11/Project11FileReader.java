9eeac67cf715c6d4cb2546e3dd5b6fe7a28830cd
Robert Dunbar <robert.dunbar@truphone.com>
2014-08-07 15:01:29 +0100

    added build.gradle, tidied code

--- Project11/src/project/e11/Project11FileReader.java
+++ Project11/src/project/e11/Project11FileReader.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package project.e11;
 
 import java.io.BufferedReader;
@@ -17,46 +16,46 @@ import java.util.StringTokenizer;
  * @author robert
  */
 public class Project11FileReader {
-    
-    public Project11FileReader(String filename, int size) {
+
+    public Project11FileReader(final String filename, int size) {
         this.filename = filename;
         this.digits = new int[size][size];
         this.size = size;
     }
-    
+
     void readFile() {
-        
+
         BufferedReader input;
-        
+
         try {
             input = new BufferedReader(new FileReader(filename));
-            
+
             String line;
             int lineNumber = 0;
             StringTokenizer st;
             while (lineNumber < size) {
                 line = input.readLine();
                 st = new StringTokenizer(line);
-                for (int i=0; i<size; i++) {
+                for (int i = 0; i < size; i++) {
                     digits[lineNumber][i] = Integer.parseInt(st.nextToken());
                 }
                 lineNumber++;
             }
-            
+
             input.close();
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println(e.toString());
-        } catch(IOException e) {
+        } catch (IOException e) {
             System.out.println(e.toString());
         }
-        
+
     }
-    
+
     int[][] getDigits() {
         return digits;
     }
-    
+
     private final String filename;
     private final int[][] digits;
-    private int size;
+    private final int size;
 }
